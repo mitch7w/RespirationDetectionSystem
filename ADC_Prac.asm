3feff7f1609b974ab45f08fafb5472fca00384c0
Mitch <mitch7w@gmail.com>
2020-06-02 22:54:56 +0200

    Got external LEDs working. Logic for ADC and breathPM extended. Timer and counting breaths started

--- ADC_Prac.asm
+++ ADC_Prac.asm
@@ -15,6 +15,8 @@
 	HIGHTHRESHOLD
 	REACHEDHIGH
 	BREATHPM
+	NUMBREATHSCOUNTED
+	AVGCALC
     endc
     ;org 08h ISR Vector
 	;GOTO ADCISR
@@ -34,13 +36,16 @@
 	    
     ;------Initialize variables-------------
     
-	    MOVLW 0x0
-	    MOVWF REACHEDHIGH ; REACHEDHIGH = 0
-	    MOVWF BREATH PM ; BREATHPM = 0
+	    CLRF REACHEDHIGH ; REACHEDHIGH = 0
+	    CLRF BREATHPM ; BREATHPM = 0
+	    CLRF NUMBREATHSCOUNTED ; numBreaths = 0
+	    CLRF AVGCALC ; avgCalc = 0
 	    
-    ;------- Setup PORTD for ADC output ----------
+    ;------- Setup PORTD for ADC LED output ----------
 	    CLRF PORTD
 	    CLRF TRISD
+	    MOVLW   b'00000000'
+	    MOVWF PORTD ; All LEDs on PORTD off
 	    
 ;------- Setup Timer2 for counting respiration rate ----------
 	    MOVLW b'00000001' ; TODO change this to correct postscalar values and whatnot
@@ -77,7 +82,54 @@ EvaluateH   MOVF HIGHTHRESHOLD,W ; Put high threshold in W for comparison
 	    CALL SRHIGH
 ADCEND	    RETFIE
 	
-	    ; -------- Subroutine to SET PIC LEDs ----------
+	    
+
+SRLOW	    MOVLW b'11111111'
+	    CPFSEQ REACHEDHIGH,W ; skip next line if reachedHigh = true
+	    BRA TIMERCHECK ; reachedHigh not = true, go to timerCheck
+	    ; reachedHigh is = true
+	    BCF TMR2CON,TMR2ON ; switch off timer
+	    MOVFW TMR2
+	    ; stop timer
+	    MOVFW TMR2; write breath period to W
+	    ADDWF AVGCALC ; AVGCALC gets new value of timer added each time
+	    MOVLW 0x01
+	    ADDLW NUMBREATHSCOUNTED; WREG++
+	    MOVWF NUMBREATHSCOUNTED ; numBreathsCounted++
+	    MOVLW 0x08
+	    CPFSLT NUMBREATHSCOUNTED ; if numBreathsCounted < 8 skip next line
+	    CALL SRAVERAGEBREATHS; numBreathsCounted = 8
+	    BRA LOWRETURN
+TIMERCHECK  
+	    MOVLW b'0'
+	    CPFSEQ TMR2ON,W ; Skip next line if timer not started TODO Check notation
+	    BRA LOWRETURN ; timer has started
+	    CLRF TMR2 ; if timer not started start it
+	    BSF T2CON,TMR2ON    
+LOWRETURN   RETURN
+	    
+SRHIGH	    MOVLW b'0'
+	    CPFSEQ TMR2ON,W ; Skip next line if timer not started TODO Check notation
+	    BSF REACHEDHIGH ; reachedHigh = true	    
+HIGHRETURN  RETURN
+	    
+	    
+SRAVERAGEBREATHS    ; Average last 8 breaths to get respiration rate
+	    ; AVGCALC contains sum of all breath periods
+	    ; divide avgcalc by 8 to get avg period
+	    ; divide 1 by result to get breath per second
+	    MOVLW 0x3c ; 60
+	    MULLW AVGCALC; multiply by 60 to get breath pm
+	    ; multiplication result stored in PRODH and PRODL
+	    MOVFW PRODL ; assuming product never uses prodH
+	    MOVWF BREATHPM; write result to BREATHPM
+	    CALL PICLEDS
+	    BCF NUMBREATHSCOUNTED ; start counting numBreaths again
+	    BCF REACHEDHIGH ; reachedHigh = false
+	    BCF AVGCALC ; the average register must be cleared
+	    RETURN 
+	    
+; -------- Subroutine to SET PIC LEDs ----------
 PICLEDS
 	; Check value of BREATHPM and set various PIC LEDs accordingly		
     Less12
@@ -109,34 +161,5 @@ Greater30
 	    MOVLW   b'00001111' ; BREATHPM is between 16 and 20
 	    MOVWF PORTD ; 4 LEDs on PORTD on
 	    RETURN ; PICLEDS Subroutine returns
-
-SRLOW	    MOVLW b'11111111'
-	    CPFSEQ REACHEDHIGH,W ; skip next line if reachedHigh = true
-	    BRA TIMERCHECK ; reachedHigh not = true, go to timerCheck
-	    ; reachedHigh is = true
-	    BCF TMR2CON,TMR2ON ; switch off timer
-	    MOVFW TMR2
-	    ; stop timer
-	    ; write breath period
-	    BCF REACHEDHIGH ; reachedHigh = false
-	    BRA LOWRETURN
-TIMERCHECK  
-	    MOVLW b'0'
-	    CPFSEQ TMR2ON,W ; Skip next line if timer not started TODO Check notation
-	    BRA LOWRETURN ; timer has started
-	    CLRF TMR2 ; if timer not started start it
-	    BSF T2CON,TMR2ON
-	    
-LOWRETURN   RETURN
-	    
-SRHIGH	    MOVLW b'0'
-	    CPFSEQ TMR2ON,W ; Skip next line if timer not started TODO Check notation
-	    BSF REACHEDHIGH ; reachedHigh = true	    
-HIGHRETURN  RETURN
-	    
-	    
-	    
-	    
-	    
 	    
 end
